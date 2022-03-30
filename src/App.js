59ef4e4537a0217b9ad702ad11f2b8f4f684ff0f
Sam Meech-Ward <sam@meech-ward.me>
2022-03-30 10:03:46 -0700

    Add another rick

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src="https://c.tenor.com/yheo1GGu3FwAAAAd/rick-roll-rick-ashley.gif" className="App-logo" alt="logo" />
+        <img src="https://c.tenor.com/yheo1GGu3FwAAAAd/rick-roll-rick-ashley.gif" className="App-logo" alt="logo" />
       </header>
     </div>
   );
